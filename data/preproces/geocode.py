a8de16a1df476aee2f75169516527d3a1a02b668
Banglanfeng Pan <panbanglanfeng@gmail.com>
2019-03-04 22:08:45 -0500

    added updated csv as well as address cache

--- data/preproces/geocode.py
+++ data/preproces/geocode.py
@@ -4,34 +4,56 @@ import sys
 import pandas as pd
 import json
 import os
+import pickle
+
+
+cached_address_file = 'cached_address'
 
 
 def add_geocode_to_data(file):
     df = pd.read_csv(file)
 
-    if 'geocode' not in df.columns:
-        df['full_address'] = df['Address']+', '+df['City']+' Ontario'
-        df['geocode'] = df['full_address'].apply(get_geocode_by_address)
+    if 'lat' not in df.columns or 'lng' not in df.columns:
+        df['lat'] = df.apply(lambda x: get_geocode_by_address(
+            x['Address'], x['City'], output='lat'), axis=1)
+        df['lng'] = df.apply(lambda x: get_geocode_by_address(
+            x['Address'], x['City'], output='lng'), axis=1)
 
     df.to_csv(file, index=False)
 
 
-def get_geocode_by_address(address):
-    print('Dealing with:'+address)
-    g = geocoder.osm(address)
-
-    if g.json is None:
-        print('Addres is None:'+address)
+def get_geocode_by_address(address, city, output='json'):
+    geocode = {
+        'lat': '',
+        'lng': '',
+    }
+    if not isinstance(address, str) or not isinstance(city, str):
         return ''
 
-    result = {
-        'lat': g.json['lat'],
-        'lng': g.json['lng'],
-    }
-    json_result = json.dumps(result)
-    # print('result==>'+json_result)
+    address = address.strip() + ', ' + city.strip() + ' Ontario'
+    address = address.lower()
+    print('Dealing with:'+address)
+    if address in address_dict:
+        print('found cache')
+        geocode = address_dict[address]
 
-    return json_result
+    else:
+        g = geocoder.osm(address)
+
+        if g.json is None:
+            print('Addres is None:'+address)
+
+        else:
+            geocode = {
+                'lat': g.json['lat'],
+                'lng': g.json['lng'],
+            }
+            address_dict[address] = geocode
+
+    if output in geocode:
+        return geocode.get(output, '')
+    else:
+        return geocode
 
 
 def create_geojson_feature(x, y, id, properties):
@@ -48,12 +70,10 @@ def create_geojson_from_csv(file):
     features = []
 
     for index, row in df.iterrows():
-        if pd.notna(row['geocode']):
-            geocode = json.loads(row['geocode'])
-            lat = geocode['lat']
-            lng = geocode['lng']
+        if pd.notna(row['lat']) and pd.notna(row['lng']):
+            lat = float(row['lat'])
+            lng = float(row['lng'])
             properties = row.fillna('').to_dict()
-            properties.pop('geocode')
             feature = create_geojson_feature(lng, lat, index, properties)
             features.append(feature)
     collects = geojson.FeatureCollection(features)
@@ -61,9 +81,24 @@ def create_geojson_from_csv(file):
         geojson.dump(collects, outfile)
 
 
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    if not os.path.exists(name + '.pkl'):
+        return {}
+
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
 if __name__ == '__main__':
     data_file = sys.argv[1]
     print("counting...")
+    address_dict = load_obj(cached_address_file)
     add_geocode_to_data(data_file)
     create_geojson_from_csv(data_file)
     print("updated file...")
+    save_obj(address_dict, cached_address_file)
