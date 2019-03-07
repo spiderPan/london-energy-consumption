9c41a5ef7d3ddcc18bd2ffc10259f22e3e4cfae2
Banglanfeng Pan <panbanglanfeng@gmail.com>
2019-03-06 22:02:18 -0500

    merge all into one and updated README

--- data/README.md
+++ data/README.md
@@ -6,3 +6,23 @@ Original data from [City of London Open Data](https://www.london.ca/city-hall/op
 - [Energy consumption 2013 (City of London facilities)](http://apps.london.ca/OpenData/Excel_Files_LatLong/Energy_Consumption_GHG_Reporting_2013.xlsx)
 - [Energy consumption 2012 (City of London facilities)](http://apps.london.ca/OpenData/Excel_Files_LatLong/Energy_Consumption_GHG_Reporting_2012.xlsx)
 - [Energy consumption 2011 (City of London facilities)](https://www.london.ca/residents/Environment/Energy/Documents/Energy%20Consumption%202011%20%28City%20of%20London%29.xls)
+
+
+## Install Requirements
+
+```
+pip3 install -r requirements.txt
+```
+
+## Usage
+Calculate Geo and convert csv to GeoJSON
+
+```
+python3 preproces/geocode.py 2011.csv
+```
+
+Merge all years into one and convert it to GeoJSON
+
+```
+python3 preproces/geocode.py merged.csv
+```
--- data/preproces/geocode.py
+++ data/preproces/geocode.py
@@ -4,6 +4,7 @@ import sys
 import pandas as pd
 import json
 import os
+import glob
 import pickle
 
 
@@ -101,8 +102,28 @@ def load_obj(name):
         return pickle.load(f)
 
 
+def merge_csvs_in_folder(path):
+    all_csvs = glob.glob(os.path.join(path, "*.csv"))
+    merged_content = []
+
+    for csv in all_csvs:
+        filename = os.path.basename(csv)
+        year = os.path.splitext(filename)[0]
+        if year == 'merged':
+            continue
+        df = pd.read_csv(csv, index_col=None, header=0)
+        df['year'] = year
+        merged_content.append(df)
+    df = pd.concat(merged_content, axis=0, ignore_index=True, sort=True)
+    df.to_csv(os.path.join(path, "merged.csv"), index=False)
+
+
 if __name__ == '__main__':
     data_file = sys.argv[1]
+
+    if data_file.startswith('merged'):
+        merge_csvs_in_folder('./csv')
+
     print("counting...")
     address_dict = load_obj(cached_address_file)
     pre_process_data(data_file)
