7a20051511eda2552087f81b923b8269eb864998
Pan <panb@tbkcreative.com>
2020-09-09 22:36:31 -0400

    load json

--- src/App.js
+++ src/App.js
@@ -14,20 +14,18 @@ const reducers = combineReducers({
 const store = createStore(reducers, {}, applyMiddleware(taskMiddleware));
 
 export default function App() {
-    return ( <
-        Provider store = { store } >
-        <
-        Map / >
-        <
-        /Provider>
+    return (
+        <Provider store={store}>
+            <Map />
+        </Provider>
     );
 }
 
 function Map() {
     const dispatch = useDispatch();
-    const { data } = useSwr("covid", async() => {
+    const { data } = useSwr("london-energy-consumption", async () => {
         const response = await fetch(
-            "https://gist.githubusercontent.com/leighhalliday/a994915d8050e90d413515e97babd3b3/raw/a3eaaadcc784168e3845a98931780bd60afb362f/covid19.json"
+            "https://raw.githubusercontent.com/spiderPan/london-energy-consumption/master/data/keplergl.json"
         );
         const data = await response.json();
         return data;
@@ -35,30 +33,20 @@ function Map() {
 
     React.useEffect(() => {
         if (data) {
+            console.log(data);
             dispatch(
-                addDataToMap({
-                    datasets: {
-                        info: {
-                            label: "COVID-19",
-                            id: "covid19"
-                        },
-                        data
-                    },
-                    option: {
-                        centerMap: true,
-                        readOnly: false
-                    },
-                    config: {}
-                })
+                addDataToMap(data)
             );
         }
     }, [dispatch, data]);
 
-    return ( <
-        KeplerGl id = "covid"
-        mapboxApiAccessToken = { process.env.REACT_APP_MAPBOX_API }
-        width = { window.innerWidth }
-        height = { window.innerHeight }
+    return (
+        <KeplerGl
+            id="london-energy-consumption"
+            // mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}
+            mapboxApiAccessToken="pk.eyJ1IjoicGFuYmFuZ2xhbmZlbmciLCJhIjoiY2tldzcwZXpwMDQ1bjJ2cGY5MGE4aGh4eiJ9.KxSVCnIYnGanlenavcPJLg"
+            width={window.innerWidth}
+            height={window.innerHeight}
         />
     );
 }
